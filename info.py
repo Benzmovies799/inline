1ab45657f7d00509e76efdeb6cf84cb7c49fcc8a
Benzmovies799 <128473684+Benzmovies799@users.noreply.github.com>
2025-02-19 12:14:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6589241609:AAEloFnJBjm2KxOByB--NSuMKI4U5w7
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://graph.org/file/2d20cb938f67aa3f94876.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://envs.sh/h7c.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/AmRobots_About/5")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
